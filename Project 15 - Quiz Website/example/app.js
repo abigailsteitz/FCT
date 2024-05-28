16acc5203df2a0f7831b932c8f56bc378a83376a
audreypettibon <audreypettibon@seattleacademy.org>
2024-05-28 09:14:12 -0700

    quiz website

--- Project 15 - Quiz Website/example/app.js
+++ Project 15 - Quiz Website/example/app.js
@@ -1,55 +1,77 @@
-// Switch to strict mode to get more useful errors when you make mistakes.
 'use strict';
 
 function resetResult() {
-    document.getElementById("LAResult").style.display = "none";
-    document.getElementById("SeattleResult").style.display = "none";
-    document.getElementById("MiamiResult").style.display = "none";
-    document.getElementById("ChicagoResult").style.display = "none";
+    document.getElementById("BelleResult").style.display = "none";
+    document.getElementById("AuroraResult").style.display = "none";
+    document.getElementById("JasmineResult").style.display = "none";
+    document.getElementById("SnowWhiteResult").style.display = "none";
 }
 
 function seeResult() {
-    var LA = 0;
-    var Seattle = 0;
-    var Miami = 0;
-    var Chicago = 0;
+    var Belle = 0;
+    var Aurora = 0;
+    var Jasmine = 0;
+    var SnowWhite = 0;
 
-    if (document.getElementById("hot").checked) {
-        LA += 1;
-        Miami += 1;
-    } else if (document.getElementById("cold").checked) {
-        Chicago += 1;
+    if (document.getElementById("Starbucks").checked) {
+        Aurora += 1;
+    } else if (document.getElementById("DunkinDonuts").checked) {
+        SnowWhite += 1;
+    } else if (document.getElementById("DutchBros").checked) {
+        Belle += 1;
     } else {
-        Seattle += 1;
+        Jasmine += 1;
     }
 
-    if (document.getElementById("bus").checked) {
-        Miami += 1;
-    } else if (document.getElementById("car").checked) {
-        LA += 1;
+    if (document.getElementById("Subway").checked) {
+        Belle += 1;
+    } else if (document.getElementById("JimmyJohns").checked) {
+        Jasmine += 1;
+    } else if (document.getElementById("JerseyMikes").checked) {
+        SnowWhite += 1;
     } else {
-        Seattle += 1;
-        Chicago += 1;
+        Aurora += 1;
     }
 
-    if (document.getElementById("walk").checked) {
-        Seattle += 1;
-        Miami += 1;
-    } else if (document.getElementById("drive").checked) {
-        LA += 1;
+    if (document.getElementById("Pagliacci").checked) {
+        SnowWhite += 1;
+    } else if (document.getElementById("Dominos").checked) {
+        Belle += 1;
+    } else if (document.getElementById("PizzaHut").checked) {
+        Jasmine += 1;
     } else {
-        Chicago += 1;
+        Aurora += 1;
+    }
+
+    if (document.getElementById("McDonalds").checked) {
+        Jasmine += 1;
+    } else if (document.getElementById("BurgerKing").checked) {
+        Aurora += 1;
+    } else if (document.getElementById("ShakeShack").checked) {
+        SnowWhite += 1;
+    } else {
+        Belle += 1;
+    }
+
+    if (document.getElementById("DairyQueen").checked) {
+        Aurora += 1;
+    } else if (document.getElementById("BenNJerrys").checked) {
+        SnowWhite += 1;
+    } else if (document.getElementById("HaagenDazs").checked) {
+        Belle += 1;
+    } else {
+        Jasmine += 1;
     }
 
     resetResult();
 
-    if (LA > Seattle && LA > Miami && LA > Chicago) {
-        document.getElementById("LAResult").style.display = "block";
-    } else if (Seattle > LA && Seattle > Miami && Seattle > Chicago) {
-        document.getElementById("SeattleResult").style.display = "block";
-    } else if (Miami > LA && Miami > Seattle && Miami > Chicago) {
-        document.getElementById("MiamiResult").style.display = "block";
+    if (Belle > SnowWhite && Belle > Jasmine && Belle > Aurora) {
+        document.getElementById("BelleResult").style.display = "block";
+    } else if (Aurora > Belle && Aurora > Jasmine && Aurora > SnowWhite) {
+        document.getElementById("AuroraResult").style.display = "block";
+    } else if (Jasmine > Belle && Jasmine > SnowWhite && Jasmine > Aurora) {
+        document.getElementById("JasmineResult").style.display = "block";
     } else {
-        document.getElementById("ChicagoResult").style.display = "block";
+        document.getElementById("SnowWhiteResult").style.display = "block";
     }
 }
