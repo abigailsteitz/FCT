69c14c4da4b1f9fcf0e71f7e229494d3ef31bb68
liamhazeltine <liamhazeltine@seattleacademy.org>
2024-05-30 14:36:16 -0700

    project 15
    project 15

--- Project 15 - Quiz Website/example/app.js
+++ Project 15 - Quiz Website/example/app.js
@@ -2,54 +2,79 @@
 'use strict';
 
 function resetResult() {
-    document.getElementById("LAResult").style.display = "none";
-    document.getElementById("SeattleResult").style.display = "none";
-    document.getElementById("MiamiResult").style.display = "none";
-    document.getElementById("ChicagoResult").style.display = "none";
+    document.getElementById("DoctorResult").style.display = "none";
+    document.getElementById("ChefResult").style.display = "none";
+    document.getElementById("ChildCareResult").style.display = "none";
+    document.getElementById("AthleteResult").style.display = "none";
 }
 
 function seeResult() {
-    var LA = 0;
-    var Seattle = 0;
-    var Miami = 0;
-    var Chicago = 0;
+    var Chef = 0;
+    var ChildCare = 0;
+    var Doctor = 0;
+    var Athlete = 0;
 
-    if (document.getElementById("hot").checked) {
-        LA += 1;
-        Miami += 1;
+    if (document.getElementById("Tall").checked) {
+        Athlete += 1;
+        
     } else if (document.getElementById("cold").checked) {
-        Chicago += 1;
+        ChildCare += 1;
     } else {
-        Seattle += 1;
+        Chef += 1;
+        Doctor += 1;
     }
 
-    if (document.getElementById("bus").checked) {
-        Miami += 1;
-    } else if (document.getElementById("car").checked) {
-        LA += 1;
+    if (document.getElementById("Pizza").checked) {
+        
+        Athlete += 1;
+    } else if (document.getElementById("steak").checked) {
+       Chef += 1;
     } else {
-        Seattle += 1;
-        Chicago += 1;
+        ChildCare += 1;
+        Doctor += 1;
+    }
+    
+    if (document.getElementById("Yes").checked) {
+        Chef += 1;
+        
+    } else if (document.getElementById("Kinda").checked) {
+        Doctor += 1;
+        ChildCare += 1;
+    } else {
+        Athlete += 1;
     }
 
-    if (document.getElementById("walk").checked) {
-        Seattle += 1;
-        Miami += 1;
-    } else if (document.getElementById("drive").checked) {
-        LA += 1;
+    if (document.getElementById("Good").checked) {
+        ChildCare += 1;
+        
+    } else if (document.getElementById("Alright").checked) {
+        Athlete += 1;
+        Doctor += 1;
     } else {
-        Chicago += 1;
+        
+        Chef += 1;
+    }
+
+    
+    if (document.getElementById("Cats").checked) {
+        Chef += 1;
+        
+    } else if (document.getElementById("Dogs").checked) {
+        ChildCare += 1;
+    } else {
+        Athlete += 1;
+        Doctor += 1;
     }
 
     resetResult();
 
-    if (LA > Seattle && LA > Miami && LA > Chicago) {
-        document.getElementById("LAResult").style.display = "block";
-    } else if (Seattle > LA && Seattle > Miami && Seattle > Chicago) {
-        document.getElementById("SeattleResult").style.display = "block";
-    } else if (Miami > LA && Miami > Seattle && Miami > Chicago) {
-        document.getElementById("MiamiResult").style.display = "block";
+    if (ChildCare > Chef && ChildCare > Doctor && ChildCare > Athlete) {
+        document.getElementById("ChildCareResult").style.display = "block";
+    } else if (Chef > ChildCare && Chef > Doctor && Chef > Athlete) {
+        document.getElementById("ChefResult").style.display = "block";
+    } else if (Doctor > ChildCare && Doctor > Chef && Doctor > Athlete) {
+        document.getElementById("DoctorResult").style.display = "block";
     } else {
-        document.getElementById("ChicagoResult").style.display = "block";
+        document.getElementById("AthleteResult").style.display = "block";
     }
 }
