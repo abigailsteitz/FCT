ed497b18c19d5e3f4ff73d0778a0fba787d939cb
saframarie <safracrawford@seattleacademy.org>
2024-05-30 14:48:45 -0700

    s
    subjects

--- Project 15 - Quiz Website/example/app.js
+++ Project 15 - Quiz Website/example/app.js
@@ -2,54 +2,54 @@
 'use strict';
 
 function resetResult() {
-    document.getElementById("LAResult").style.display = "none";
-    document.getElementById("SeattleResult").style.display = "none";
-    document.getElementById("MiamiResult").style.display = "none";
-    document.getElementById("ChicagoResult").style.display = "none";
+    document.getElementById("EnglishResult").style.display = "none";
+    document.getElementById("ArtResult").style.display = "none";
+    document.getElementById("ScienceResult").style.display = "none";
+    document.getElementById("MathResult").style.display = "none";
 }
 
 function seeResult() {
-    var LA = 0;
-    var Seattle = 0;
-    var Miami = 0;
-    var Chicago = 0;
+    var English = 0;
+    var Art = 0;
+    var Science = 0;
+    var Math = 0;
 
-    if (document.getElementById("hot").checked) {
-        LA += 1;
-        Miami += 1;
-    } else if (document.getElementById("cold").checked) {
-        Chicago += 1;
+    if (document.getElementById("hands-on").checked) {
+        Art += 1;
+        Science += 1;
+    } else if (document.getElementById("lecture").checked) {
+        English += 1;
     } else {
-        Seattle += 1;
+        Math += 1;
     }
 
-    if (document.getElementById("bus").checked) {
-        Miami += 1;
-    } else if (document.getElementById("car").checked) {
-        LA += 1;
+    if (document.getElementById("write").checked) {
+        English += 1;
+    } else if (document.getElementById("draw").checked) {
+        Art += 1;
     } else {
-        Seattle += 1;
-        Chicago += 1;
+        Science += 1;
+        Math += 1;
     }
 
-    if (document.getElementById("walk").checked) {
-        Seattle += 1;
-        Miami += 1;
-    } else if (document.getElementById("drive").checked) {
-        LA += 1;
+    if (document.getElementById("red").checked) {
+        Art += 1;
+        Math += 1;
+    } else if (document.getElementById("blue").checked) {
+        English += 1;
     } else {
-        Chicago += 1;
+        Science += 1;
     }
 
     resetResult();
 
-    if (LA > Seattle && LA > Miami && LA > Chicago) {
-        document.getElementById("LAResult").style.display = "block";
-    } else if (Seattle > LA && Seattle > Miami && Seattle > Chicago) {
-        document.getElementById("SeattleResult").style.display = "block";
-    } else if (Miami > LA && Miami > Seattle && Miami > Chicago) {
-        document.getElementById("MiamiResult").style.display = "block";
+    if (English > Math && English > Science && English > Art) {
+        document.getElementById("EnglishResult").style.display = "block";
+    } else if (Art > English && Art > Science && Art > Math) {
+        document.getElementById("ArtResult").style.display = "block";
+    } else if (Math > English && Math > Science && Math > Art) {
+        document.getElementById("MathResult").style.display = "block";
     } else {
-        document.getElementById("ChicagoResult").style.display = "block";
+        document.getElementById("ScienceResult").style.display = "block";
     }
 }
