c49b3faf0fefdb35616132c6389b199aebebf9fb
tylerhuntseattleacademy <tylerhunt@seattleacademy.org>
2024-05-31 14:59:58 -0700

    warhammer quiz

--- Project 15 - Quiz Website/example/app.js
+++ Project 15 - Quiz Website/example/app.js
@@ -2,54 +2,159 @@
 'use strict';
 
 function resetResult() {
-    document.getElementById("LAResult").style.display = "none";
-    document.getElementById("SeattleResult").style.display = "none";
-    document.getElementById("MiamiResult").style.display = "none";
-    document.getElementById("ChicagoResult").style.display = "none";
+    document.getElementById("BloodaResult").style.display = "none";
+    document.getElementById("SalamanderResult").style.display = "none";
+    document.getElementById("KhorneResult").style.display = "none";
+    document.getElementById("SpaceWResult").style.display = "none";
+    document.getElementById("IwarriorResult").style.display = "none";
+    document.getElementById("NightResult").style.display = "none";
+    document.getElementById("UltraResult").style.display = "none";
+    document.getElementById("WhiteResult").style.display = "none";
+    document.getElementById("BlackLResult").style.display = "none";
+    document.getElementById("DeathgResult").style.display = "none";
+    document.getElementById("DarkAResult").style.display = "none";
+    document.getElementById("AlphaResult").style.display = "none";
 }
 
 function seeResult() {
-    var LA = 0;
-    var Seattle = 0;
-    var Miami = 0;
-    var Chicago = 0;
+    var Blooda = 0;
+    var Salamander = 0;
+    var Khorne = 0;
+    var SpaceW = 0;
+    var Iwarriors = 0;
+    var Night = 0;
+    var Ultra = 0;
+    var White = 0;
+    var BlackL = 0;
+    var Deathg = 0;
+    var DarkA = 0;
+    var Alpha = 0;
 
     if (document.getElementById("hot").checked) {
-        LA += 1;
-        Miami += 1;
+        Blooda += 1;
+        Salamander += 1;
+        Khorne += 1;
+        Deathg += 1;
     } else if (document.getElementById("cold").checked) {
-        Chicago += 1;
+        SpaceW += 1;
+        Night += 1;
+        Alpha += 1;
     } else {
-        Seattle += 1;
+        Iwarriors += 1;
+        Ultra += 1;
+        White += 1;
+        BlackL += 1;
+        DarkA += 1;
     }
 
-    if (document.getElementById("bus").checked) {
-        Miami += 1;
-    } else if (document.getElementById("car").checked) {
-        LA += 1;
+    if (document.getElementById("Axe").checked) {
+        Khorne += 1;
+        SpaceW += 1;
+        Deathg += 1;
+    } else if (document.getElementById("Gun").checked) {
+        Salamander += 1;
+        Iwarriors += 1;
     } else {
-        Seattle += 1;
-        Chicago += 1;
+        Blooda += 1;
+        Night += 1;
+        Ultra += 1;
+        White += 1;
+        BlackL += 1;
+        DarkA += 1;
+        Alpha += 1;
     }
 
-    if (document.getElementById("walk").checked) {
-        Seattle += 1;
-        Miami += 1;
-    } else if (document.getElementById("drive").checked) {
-        LA += 1;
+    if (document.getElementById("Offence").checked) {
+        Khorne += 1;
+        SpaceW += 1;
+        Blooda += 1;
+        Night += 1;
+        White += 1;
+    } else if (document.getElementById("Defence").checked) {
+        Deathg += 1;
+        Iwarriors += 1;
+        DarkA += 1;
     } else {
-        Chicago += 1;
+        Salamander += 1;
+        Ultra += 1;
+        BlackL += 1;
+        Alpha += 1;
     }
+
+    if (document.getElementById("Good").checked) {
+        DarkA += 2;
+        SpaceW += 2;
+        Blooda += 2;
+        Salamander += 2;
+        Ultra += 2;
+        White += 2;
+    } else if (document.getElementById("Bad").checked) {
+        Deathg += 2;
+        Iwarriors += 2;
+        Khorne += 2;
+        Night += 2;
+        BlackL += 2;
+        Alpha += 2;
+    }
+
+    if (document.getElementById("Red").checked) {
+        Blooda += 2;
+        Khorne += 2;
+    } else if (document.getElementById("Blue").checked) {
+        Night += 2;
+        Alpha += 2;
+        SpaceW += 2;
+        Ultra += 2;
+    } else if (document.getElementById("Green").checked) {
+        DarkA += 2;
+        Deathg += 2;
+        Salamander += 2;
+    } else if (document.getElementById("White").checked) {
+        White += 2;
+    } else if (document.getElementById("Black").checked) {
+        Iwarriors += 2;
+        BlackL += 2;
+    }
+
 
     resetResult();
 
-    if (LA > Seattle && LA > Miami && LA > Chicago) {
-        document.getElementById("LAResult").style.display = "block";
-    } else if (Seattle > LA && Seattle > Miami && Seattle > Chicago) {
-        document.getElementById("SeattleResult").style.display = "block";
-    } else if (Miami > LA && Miami > Seattle && Miami > Chicago) {
-        document.getElementById("MiamiResult").style.display = "block";
-    } else {
-        document.getElementById("ChicagoResult").style.display = "block";
+    let max = Math.max(Khorne,SpaceW,Blooda,Night,White,Deathg,Iwarriors,DarkA,Salamander,Ultra,BlackL,Alpha)
+    if(max==Khorne){
+        document.getElementById("KhorneResult").style.display = "block";
+    }
+    else if(max==SpaceW){
+        document.getElementById("SpaceWResult").style.display = "block";
+    }
+    else if(max==Blooda){
+        document.getElementById("BloodaResult").style.display = "block";
+    }
+    else if(max==Night){
+        document.getElementById("NightResult").style.display = "block";
+    }
+    else if(max==White){
+        document.getElementById("WhiteResult").style.display = "block";
+    }
+    else if(max==Deathg){
+        document.getElementById("DeathgResult").style.display = "block";
+    }
+    else if(max==Iwarriors){
+        document.getElementById("IwarriorResult").style.display = "block";
+    }
+    else if(max==DarkA){
+        document.getElementById("DarkAResult").style.display = "block";
+    }
+    else if(max==Salamander){
+        document.getElementById("SalamanderResult").style.display = "block";
+    }
+    else if(max==Ultra){
+        document.getElementById("UltraResult").style.display = "block";
+    }
+    else if(max==BlackL){
+        document.getElementById("BlackLResult").style.display = "block";
+    }
+    else if(max==Alpha){
+        document.getElementById("AlphaResult").style.display = "block";
     }
 }
+
