6ab286ab16bb0c6ff04e00b08963522593c82554
AnnikaCAnnikaC <annikachesnutt@seattleacademy.org>
2024-06-03 08:38:31 -0700

    quiz

--- Project 15 - Quiz Website/example/app.js
+++ Project 15 - Quiz Website/example/app.js
@@ -2,54 +2,77 @@
 'use strict';
 
 function resetResult() {
-    document.getElementById("LAResult").style.display = "none";
-    document.getElementById("SeattleResult").style.display = "none";
-    document.getElementById("MiamiResult").style.display = "none";
-    document.getElementById("ChicagoResult").style.display = "none";
+    document.getElementById("PlasticResult").style.display = "none";
+    document.getElementById("WoodenResult").style.display = "none";
+    document.getElementById("MetalResult").style.display = "none";
+    document.getElementById("TableResult").style.display = "none";
 }
 
 function seeResult() {
-    var LA = 0;
-    var Seattle = 0;
-    var Miami = 0;
-    var Chicago = 0;
+    var Plastic = 0;
+    var Wooden = 0;
+    var Metal = 0;
+    var Table = 0;
 
-    if (document.getElementById("hot").checked) {
-        LA += 1;
-        Miami += 1;
-    } else if (document.getElementById("cold").checked) {
-        Chicago += 1;
+    if (document.getElementById("summ").checked) {
+        Plastic += 1;
+    } else if (document.getElementById("wint").checked) {
+        Wooden += 1;
+    } else if (document.getElementById("spring").checked) {
+        Metal += 1;
     } else {
-        Seattle += 1;
+        Table += 1;
     }
 
-    if (document.getElementById("bus").checked) {
-        Miami += 1;
-    } else if (document.getElementById("car").checked) {
-        LA += 1;
+    if (document.getElementById("Break").checked) {
+        Plastic += 1;
+    } else if (document.getElementById("Lunch").checked) {
+        Metal += 1;
+    } else if (document.getElementById("Dinner").checked) {
+        Wooden += 1;
     } else {
-        Seattle += 1;
-        Chicago += 1;
+        Table += 1;
     }
 
-    if (document.getElementById("walk").checked) {
-        Seattle += 1;
-        Miami += 1;
-    } else if (document.getElementById("drive").checked) {
-        LA += 1;
+    if (document.getElementById("blond").checked) {
+        Plastic += 1;
+    } else if (document.getElementById("brown").checked) {
+        Wooden += 1;
+    } else if (document.getElementById("black").checked) {
+            Metal += 1;
     } else {
-        Chicago += 1;
+        Table += 1;
+    }
+    if (document.getElementById("red").checked) {
+        Plastic += 1;
+    } else if (document.getElementById("yellow").checked) {
+        Wooden += 1;
+    } else if (document.getElementById("blue").checked) {
+            Metal += 1;
+    } else {
+        Table += 1;   
+    }
+     if (document.getElementById("blob").checked) {
+        Plastic += 1;
+    } else if (document.getElementById("trout").checked) {
+        Wooden += 1;
+    } else if (document.getElementById("rock").checked) {
+            Metal += 1;
+    } else {
+        Table += 1;
+
+     
     }
 
     resetResult();
 
-    if (LA > Seattle && LA > Miami && LA > Chicago) {
-        document.getElementById("LAResult").style.display = "block";
-    } else if (Seattle > LA && Seattle > Miami && Seattle > Chicago) {
-        document.getElementById("SeattleResult").style.display = "block";
-    } else if (Miami > LA && Miami > Seattle && Miami > Chicago) {
-        document.getElementById("MiamiResult").style.display = "block";
+    if (Plastic > Wooden && Plastic > Metal && Plastic > Table) {
+        document.getElementById("PlasticResult").style.display = "block";
+    } else if (Wooden > Plastic && Wooden > Metal && Wooden > Table) {
+        document.getElementById("WoodenResult").style.display = "block";
+    } else if (Metal > Plastic && Metal > Wooden && Metal > Table) {
+        document.getElementById("MetalResult").style.display = "block";
     } else {
-        document.getElementById("ChicagoResult").style.display = "block";
+        document.getElementById("TableResult").style.display = "block";
     }
 }
